a9c97832bea77e6df7e41530bef83468c27a9769
greenMakaroni <mrtn.dawid@gmail.com>
2022-06-20 18:32:23 +0100

    get clients, delete client, use cache to reload after delete

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { FaTrash } from 'react-icons/fa';
+import { useMutation } from '@apollo/client';
+import { DELETE_CLIENT } from '../mutations/clientMutations'
+import { GET_CLIENTS } from '../queries/clientQueries';
 
 export default function ClientRow({ id, client }) {
+ 
+  const [deleteClient] = useMutation(DELETE_CLIENT, {
+    variables: { id: client.id },
+    update(cache, {data: { deleteClient }}) {
+        const { clients } = cache.readQuery({ query: GET_CLIENTS });
+        cache.writeQuery({
+            query: GET_CLIENTS,
+            data: { clients: clients.filter(client => client.id !== deleteClient.id)},
+        });
+    }
+  });
+
   return (
-    <div>{ client.name }</div>
+    <tr>
+        <td> { client.name } </td>
+        <td> { client.email } </td>
+        <td> {client.phone } </td>
+        <td>
+            <button className="btn btn-danger btn-sm" onClick={deleteClient}>
+                <FaTrash />
+            </button>
+        </td>
+    </tr>
   )
 }
